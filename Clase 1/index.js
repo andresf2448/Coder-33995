1c2b38330c9ce2ffd0dfce588559edd93e2a9791
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-10 20:32:52 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,54 @@
+//comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+/* //forma de declarar variables no recomendada
+var edad;
+
+//declaramos nuestra primer variable (declaración de variable)
+let nombre = "andres";
+let apellido;
+const EDAD = 15;
+
+//asignación de apellido
+apellido = "andres"; */
+
+//Ejemplo
+// let numeroUno = 2;
+// let numeroDos = 10;
+// const NUMEROTRES = 20;
+
+// let suma = numeroUno + numeroDos;
+// console.log("Aca vemos el valor de la suma", suma * NUMEROTRES);
+
+// let saludo = "Hola ";
+// let nombre = "Camilo";
+
+// let mensaje = saludo + nombre;
+// console.log(mensaje);
+
+// let saludo = "Hola ";
+// let nombre = prompt("Ingrese su nombre");
+// let mensaje = saludo + nombre;
+
+// console.log(mensaje);
+
+// let NumeroUno = parseInt(prompt("Ingrese un numero"));
+// let NumeroDos = parseInt(prompt("Ingrese otro numero"));
+
+// let suma = NumeroDos + NumeroUno;
+
+// alert(suma);
+
+
+let nombre = "andres";
+let saludo = "Hola";
+
+// let mensaje = saludo + " " + nombre;
+let mensaje = `${saludo} senor ${nombre}`;
+
+
+console.log(mensaje);
