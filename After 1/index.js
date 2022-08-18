318e46448a1444bcc2c9ee953b1c6bb6cb071337
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-08-18 18:03:40 -0500

    After 1

---
+++ After 1/index.js
@@ -0,0 +1,40 @@
+/*
+variables y constantes
+let nombre = "Camila";
+const EDAD = 15;
+*/
+
+/* let nombre = prompt("Ingrese su nombre");
+
+if(nombre === "fabian"){
+  alert("Hola fabian");
+}else{
+  alert("Quien eres?");
+} */
+
+/* let edad = parseInt(prompt("Ingresa tu edad"));
+
+if(edad < 10){
+  alert("eres muy peque");
+}else if(edad < 18){
+  alert("Ya estas grande");
+}else{
+  alert("ya eres mayor");
+} */
+
+/* for(let i = 20; i < 100; i = i + 5){
+  console.log(i);
+} */
+let nombre = prompt("ingrese el nombre");
+
+while (nombre === ""){
+  alert("Ingresa un nombre valido");
+  nombre = prompt("ingrese el nombre");
+}
+
+alert("Bienvenid@");
+
+/* 
+pensar como hacer una algoritmo que diga si un numero es par o impar
+pensar como hacer un algoritmo que diga si un numero es primo o no
+*/
