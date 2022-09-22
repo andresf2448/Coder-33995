2508fb3b8375d79de2e0d7f1b98aa874f975b552
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-09-21 19:02:39 -0500

    Clase 13

---
+++ Clase 13/index.js
@@ -0,0 +1,85 @@
+let boton = document.getElementById("boton");
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Error!",
+    text: "Do you want to continue?",
+    icon: "error",
+    confirmButtonText: "Cool",
+  });
+}); */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Genial!",
+    text: "Haz agregado al carrito",
+    icon: "Exito",
+    confirmButtonText: "Bien!",
+  });
+});
+ */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Error!",
+    text: "Error inesperado",
+    icon: "error",
+    confirmButtonText: " =( ",
+  });
+});
+ */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 3000,
+  });
+}); */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Sweet!",
+    text: "Modal with a custom image.",
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image",
+  });
+}); */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Está seguro de eliminar el producto?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, seguro",
+    cancelButtonText: "No, no quiero",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      //logica para eliminar
+      Swal.fire({
+        title: "Borrado!",
+        icon: "success",
+        text: "El archivo ha sido borrado",
+      });
+    }
+  });
+}); */
+
+/* boton.addEventListener("click", () => {
+  Swal.fire({
+    title: "Submit your Github username",
+    input: "text",
+    showCancelButton: true,
+    confirmButtonText: "Look up",
+    showLoaderOnConfirm: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value}`,
+      });
+    }
+  });
+}); */
